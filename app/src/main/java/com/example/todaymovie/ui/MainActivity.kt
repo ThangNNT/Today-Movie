6006f5a03922e6f8a75a28f0b2dfe535dccc021b
thangnguyen <thangnguyen@okxe.vn>
2021-03-22 10:25:51 +0700

    create hide/show Keyboard function

--- app/src/main/java/com/example/todaymovie/ui/MainActivity.kt
+++ app/src/main/java/com/example/todaymovie/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.todaymovie.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import com.example.todaymovie.R
 import com.example.todaymovie.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,5 +12,19 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun hideKeyboard() {
+        val view = this.currentFocus
+        view?.let {
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+        }
+    }
+
+    fun showKeyBoard() {
+        val imm =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
     }
 }
