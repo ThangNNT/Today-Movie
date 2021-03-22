da411a4c60adcfb86efb4d22b5b28d43b7bffafd
thangnguyen <thangnguyen@okxe.vn>
2021-03-22 11:53:15 +0700

    setup navigation component with bottom navigation view

--- app/src/main/java/com/example/todaymovie/ui/MainActivity.kt
+++ app/src/main/java/com/example/todaymovie/ui/MainActivity.kt
@@ -1,30 +1,58 @@
 package com.example.todaymovie.ui
 
-import android.content.Context
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.todaymovie.R
 import com.example.todaymovie.ui.base.BaseActivity
+import com.example.todaymovie.util.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun hideKeyboard() {
-        val view = this.currentFocus
-        view?.let {
-            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+        if(savedInstanceState ==null){
+            setupBottomNavigationBar()
         }
     }
 
-    fun showKeyBoard() {
-        val imm =
-                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
+
+    /**
+     * Called on first creation and when restoring state.
+     */
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        val navGraphIds = listOf(R.navigation.nav_movie, R.navigation.nav_tv)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
+        )
+
+        // Whenever the selected controller changes, setup the action bar.
+        controller.observe(this, { navController ->
+            setupActionBarWithNavController(navController)
+        })
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 }
--- app/src/main/java/com/example/todaymovie/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/todaymovie/ui/base/BaseActivity.kt
@@ -1,6 +1,22 @@
 package com.example.todaymovie.ui.base
 
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 
 open class BaseActivity: AppCompatActivity() {
+
+    fun hideKeyboard() {
+        val view = this.currentFocus
+        view?.let {
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+        }
+    }
+
+    fun showKeyBoard() {
+        val imm =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+    }
 }
