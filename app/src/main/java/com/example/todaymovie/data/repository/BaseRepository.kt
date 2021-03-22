dea5065b87404c3ff971b56cba63993c946bfe26
thangnguyen <thangnguyen@okxe.vn>
2021-03-22 16:50:50 +0700

    create base map function

--- app/src/main/java/com/example/todaymovie/data/repository/BaseRepository.kt
+++ app/src/main/java/com/example/todaymovie/data/repository/BaseRepository.kt
@@ -1,4 +1,20 @@
 package com.example.todaymovie.data.repository
 
+import com.example.todaymovie.data.Result
+
 open class BaseRepository {
+
+    protected fun <I,O> Result<I>.mapTo(map:(I)->O): Result<O>{
+        return if(this.status==Result.Status.LOADING){
+            Result.loading(null)
+        } else if(this.status == Result.Status.ERROR || this.data==null){
+            Result.error(
+                message = this.message.orEmpty(),
+                errorCode = this.errorCode,
+                error = null
+            )
+        } else {
+            Result.success(map(this.data))
+        }
+    }
 }
