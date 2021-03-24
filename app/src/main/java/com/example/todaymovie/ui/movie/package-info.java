62e4a5c6052674fd400b77674ca36138c9b0d11a
thangnguyen <thangnguyen@okxe.vn>
2021-03-24 16:36:05 +0700

    apply parcelize plugin

---
+++ app/src/main/java/com/example/todaymovie/ui/movie/package-info.java
@@ -0,0 +1,7 @@
+package com.example.todaymovie.ui.movie;
+
+import com.airbnb.epoxy.EpoxyDataBindingPattern;
+import com.example.todaymovie.R;
+
+@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "item")
+interface Config {}
