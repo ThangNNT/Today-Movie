b182e09b7ae948c0cf9f7868464ed30ef511c7ae
thangnguyen <thangnguyen@okxe.vn>
2021-03-24 15:13:46 +0700

    create some dimension

--- app/src/main/java/com/example/todaymovie/util/binding_adapter/ImageBindingAdapter.kt
+++ app/src/main/java/com/example/todaymovie/util/binding_adapter/ImageBindingAdapter.kt
@@ -1,2 +1,20 @@
 package com.example.todaymovie.util.binding_adapter
 
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.example.todaymovie.R
+import com.github.ybq.android.spinkit.style.Circle
+
+
+@BindingAdapter("loadImage")
+fun loadImage(view: ImageView, url: String?){
+    val placeHolder = Circle().also {
+        it.color = ContextCompat.getColor(view.context, R.color.colorOnPrimary)
+    }
+    Glide.with(view)
+            .load(url)
+            .placeholder(placeHolder)
+            .into(view)
+}
