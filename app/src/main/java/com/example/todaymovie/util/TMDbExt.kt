70787fbe9c39b59c490121ddace0c3a5cccec6d9
thangnguyen <thangnguyen@okxe.vn>
2021-03-25 11:10:03 +0700

    add getBackDropPath function

--- app/src/main/java/com/example/todaymovie/util/TMDbExt.kt
+++ app/src/main/java/com/example/todaymovie/util/TMDbExt.kt
@@ -1,10 +1,18 @@
 package com.example.todaymovie.util
 
-const val BASE_IMAGE_URL = "https://image.tmdb.org/t/p/"
-const val IMAGE_SIZE = "original"
+private const val BASE_IMAGE_URL = "https://image.tmdb.org/t/p/"
+private const val ORIGIN_IMAGE_SIZE = "original"
+private const val W780_IMAGE_SIZE = "w780"
+private const val W1280_IMAGE_SIZE = "w1280"
 
 fun getPosterPath(imagePath: String?): String?{
     imagePath?.let {
-        return "$BASE_IMAGE_URL${IMAGE_SIZE}${imagePath}"
+        return "$BASE_IMAGE_URL${ORIGIN_IMAGE_SIZE}${imagePath}"
+    }?: return null
+}
+
+fun getBackDropPath(imagePath: String?): String?{
+    imagePath?.let {
+        return "$BASE_IMAGE_URL${W1280_IMAGE_SIZE}${imagePath}"
     }?: return null
 }
