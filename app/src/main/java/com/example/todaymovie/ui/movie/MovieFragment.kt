44e144312a734046d9842b31f7f5a77844ba6b02
thangnguyen <thangnguyen@okxe.vn>
2021-03-25 14:14:12 +0700

    apply skeleton lib

--- app/src/main/java/com/example/todaymovie/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/example/todaymovie/ui/movie/MovieFragment.kt
@@ -1,15 +1,18 @@
 package com.example.todaymovie.ui.movie
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.ethanhua.skeleton.Skeleton
+import com.ethanhua.skeleton.SkeletonScreen
+import com.example.todaymovie.R
 import com.example.todaymovie.databinding.FragmentMovieBinding
 import com.example.todaymovie.domain.model.DomainResult
 import com.example.todaymovie.domain.model.Movie
+import com.example.todaymovie.util.px
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
@@ -19,6 +22,7 @@ class MovieFragment : Fragment() {
     private lateinit var binding: FragmentMovieBinding
     private val movieViewModel: MovieViewModel by viewModels()
     private var controller: MovieController?= null
+    private var skeletonScreen: SkeletonScreen? = null
 
 
     override fun onCreateView(
@@ -32,6 +36,7 @@ class MovieFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setup()
+        setupSkeleton()
         setupObserver()
     }
 
@@ -44,14 +49,25 @@ class MovieFragment : Fragment() {
         }
     }
 
+    private fun setupSkeleton(){
+        skeletonScreen = Skeleton.bind(binding.rvMovies)
+                .adapter(binding.rvMovies.adapter)
+                .count(1)
+                .color(R.color.shimmer_color)
+                .load(R.layout.item_home_skeleton)
+                .show()
+    }
+
     private fun setupObserver(){
         movieViewModel.movieHome.observe(viewLifecycleOwner){
             when(it.status){
                 DomainResult.Status.LOADING -> {
-                    binding.loadingBar.visibility = View.VISIBLE
                 }
                 DomainResult.Status.SUCCESS -> {
-                    binding.loadingBar.visibility = View.GONE
+                    skeletonScreen?.hide()
+                    binding.rvMovies.apply {
+                        binding.rvMovies.setPadding(paddingLeft, paddingTop, paddingRight, 24.px)
+                    }
                     it.data?.let { movieHome ->
                         controller?.setData(movieHome)
                     }
