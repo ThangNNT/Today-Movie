2f1adcb565e7abc8991fa3368d6ba1a7c7228a3c
thangnguyen <thangnguyen@okxe.vn>
2021-03-25 14:54:28 +0700

    create get TV api

--- app/src/main/java/com/example/todaymovie/data/remote/MovieService.kt
+++ app/src/main/java/com/example/todaymovie/data/remote/MovieService.kt
@@ -1,6 +1,7 @@
 package com.example.todaymovie.data.remote
 
 import com.example.todaymovie.data.remote.response.MovieResponse
+import com.example.todaymovie.data.remote.response.TvResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -24,5 +25,20 @@ interface MovieService {
 
     @GET("3/movie/now_playing")
     suspend fun getNowPlayingMovies(): Response<MovieResponse>
+
+    @GET("3/tv/popular")
+    suspend fun getPopularTVs(): Response<TvResponse>
+
+    @GET("3/tv/top_rated")
+    suspend fun getTopRatedTVs(): Response<TvResponse>
+
+    @GET("3/tv/on_the_air")
+    suspend fun getOnTheAirTVs(): Response<TvResponse>
+
+    @GET("3/tv/airing_today")
+    suspend fun getAiringToday(): Response<TvResponse>
+
+    @GET("3/trending/tv/{time_window}")
+    suspend fun getTrending(@Path("time_window") windowTime: String): Response<TvResponse>
 
 }
